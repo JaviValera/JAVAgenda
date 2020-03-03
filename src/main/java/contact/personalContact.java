3b35d364dbcd9ce742c44d79452eaadd2e827654
javi <jvbvalera@hotmail.com>
2020-03-03 19:38:14 +0100

    POM with Junit and personalContact Fixed

--- src/main/java/contact/personalContact.java
+++ src/main/java/contact/personalContact.java
@@ -5,25 +5,16 @@ import java.util.Date;
 import java.util.Objects;
 public class personalContact extends contact {
 Date date;
-    public personalContact(String id, String name, String gender, String phoneNumber, String date) throws ParseException {
+    public personalContact(String id, String name, String gender, String phoneNumber, Date date) {
     super(id,name,gender,phoneNumber);
-    this.date =  new SimpleDateFormat("dd/MM/yyyy").parse(date);
+    this.date =  date;
     }
     public Date getDate() {
         return date;
     }
-    public void setDate(String date) throws ParseException {
-        this.date = new SimpleDateFormat("dd/MM/yyyy").parse(date);
-    }
-    public boolean equals(Object object) {
-        if (this == object) return true;
-        if (object == null || getClass() != object.getClass()) return false;
-        if (!super.equals(object)) return false;
-        personalContact that = (personalContact) object;
-        return java.util.Objects.equals(date, that.date);
-    }
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), date);
+
+    public void setDate(Date date) {
+        this.date = date;
     }
     public java.lang.String toString() {
         return "personalContact{" +
