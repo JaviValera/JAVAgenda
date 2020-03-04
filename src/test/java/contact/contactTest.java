66579bbae314a1df2da43971aff233c6110ffa83
javi <jvbvalera@hotmail.com>
2020-03-04 18:28:30 +0100

    tests for contact

--- src/test/java/contact/contactTest.java
+++ src/test/java/contact/contactTest.java
@@ -3,53 +3,62 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 class contactTest {
+    contact contact;
+    private int id = 1;
+    private String name = "testname";
+    private String gender = "testgender";
+    private int phoneNumber = 1;
     @BeforeEach
     void setUp() {
-
+    contact = new contact(id,name,gender,phoneNumber);
     }
 
     @Test
     void getId() {
-
+    assertEquals(id,contact.getId());
     }
 
     @Test
     void setId() {
+    id=2;
+    contact.setId(id);
+    assertEquals(id,contact.getId());
     }
 
     @Test
     void getName() {
+    assertEquals(name,contact.getName());
     }
 
     @Test
     void setName() {
+    name= "nametest";
+    contact.setName(name);
+    assertEquals(name,contact.getName());
     }
 
     @Test
     void getGender() {
+    assertEquals(gender,contact.getGender());
     }
 
     @Test
     void setGender() {
+    gender = "gendertest";
+    contact.setGender(gender);
+    assertEquals(gender,contact.getGender());
     }
 
     @Test
     void getPhoneNumber() {
+        assertEquals(phoneNumber,contact.getPhoneNumber());
     }
 
     @Test
     void setPhoneNumber() {
+        phoneNumber = 2;
+        contact.setPhoneNumber(phoneNumber);
+        assertEquals(phoneNumber,contact.getPhoneNumber());
     }
 
-    @Test
-    void testEquals() {
-    }
-
-    @Test
-    void testHashCode() {
-    }
-
-    @Test
-    void testToString() {
-    }
 }
