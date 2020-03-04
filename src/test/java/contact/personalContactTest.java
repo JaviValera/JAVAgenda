3aeff2c83787a92f7676986494111d27496573bf
javi <jvbvalera@hotmail.com>
2020-03-04 18:49:51 +0100

    tests for contacts

--- src/test/java/contact/personalContactTest.java
+++ src/test/java/contact/personalContactTest.java
@@ -3,22 +3,42 @@ package contact;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class personalContactTest {
+class personalContactTest  {
+    personalContact personal;
+    private int id;
+    private String name;
+    private String gender;
+    private int phoneNumber;
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+    private Date date;
     @BeforeEach
-    void setUp() {
-
+    void setUp() throws ParseException {
+        id=1;
+        name="testname";
+        gender="testgender";
+        phoneNumber=1;
+        date = sdf.parse("06/11/1996");
+        personal = new personalContact(id,name,gender,phoneNumber,date);
     }
     @Test
     void getDate() {
+        assertEquals(date,personal.getDate());
     }
 
     @Test
     void setDate() {
-    }
-
-    @Test
-    void testToString() {
+        try {
+            date = sdf.parse("06/11/1996");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        personal.setDate(date);
+        assertEquals(date,personal.getDate());
     }
 }
--- src/test/java/contact/professionalContactTest.java
+++ src/test/java/contact/professionalContactTest.java
@@ -6,27 +6,29 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class professionalContactTest {
+    professionalContact professional;
+    private int id;
+    private String name;
+    private String gender;
+    private int phoneNumber;
+    private String email;
     @BeforeEach
     void setUp() {
-
+    id=1;
+    name="testname";
+    gender="testgender";
+    phoneNumber=1;
+    email="testemail";
+    professional = new professionalContact(id,name,gender,phoneNumber,email);
     }
     @Test
     void getEmail() {
+        assertEquals(email,professional.getEmail());
     }
-
     @Test
     void setEmail() {
-    }
-
-    @Test
-    void testEquals() {
-    }
-
-    @Test
-    void testHashCode() {
-    }
-
-    @Test
-    void testToString() {
+        email="emailtest";
+        professional.setEmail(email);
+        assertEquals("emailtest",professional.getEmail());
     }
 }
