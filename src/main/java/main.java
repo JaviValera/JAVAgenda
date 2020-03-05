d8a7eb978930259fab342c80d101b2e313493e86
sergy <sergy_lopez_21@hotmail.com>
2020-03-05 20:03:22 +0100

    main

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -9,6 +9,7 @@ import java.util.Date;
 public class main {
     public static void main(String[] args) throws IOException {
         agenda agenda = new agenda();
+        agenda agenda1= new agenda();
         personalContact per1 = new personalContact(1,"Mikel","Male",987654321, "06/11/1996");
         personalContact per3 = new personalContact(2,"James","Female",12345678, "15/07/1990");
         personalContact per2 = new personalContact(3,"Tyler","Other",12341234, "24/02/2000");
@@ -17,15 +18,26 @@ public class main {
         professionalContact pro3 = new professionalContact(6,"Denyk","Other",90909090, "denyk@misfits.pro");
         agenda.addPersonalContact(per1);
         agenda.addPersonalContact(per2);
-        agenda.addPersonalContact(per3);
+        agenda1.addPersonalContact(per3);
         agenda.addProfessionalContact(pro1);
         agenda.addProfessionalContact(pro2);
-        agenda.addProfessionalContact(pro3);
+        agenda1.addProfessionalContact(pro3);
         agenda.savePersonalContact();
         agenda.saveProfessionalContact();
         agenda.generatePersonalContactsFile();
         agenda.generateProfessionalContactsFile();
         System.out.println(agenda.searchContact(pro1).toString());
+        agenda.modify(pro1,1,"99");
+        System.out.println(agenda.searchContact(pro1).toString());
+        agenda.modify(pro1,2,"pepe");
+        System.out.println(agenda.searchContact(pro1).toString());
+        agenda.modify(pro1,3,"tanque apache");
+        System.out.println(agenda.searchContact(pro1).toString());
+        agenda.modify(pro1,4,"112");
+        System.out.println(agenda.searchContact(pro1).toString());
+        agenda.modify(pro1,0,"pepe");
+        System.out.println(agenda.searchContact(pro1).toString());
+        //agenda.mergeAgenda(agenda1);
         agenda.wipe();
     }
 }
